972a3c4e6de58f2433de42411f35a500cfed597c
Jay <jburton6@aum.edu>
2023-12-20 22:37:22 -0600

    add handle takes to profile // add copy did to clipboard.

--- src/detail-panels/account-header.js
+++ src/detail-panels/account-header.js
@@ -1,7 +1,7 @@
 // @ts-check
 /// <reference path="../types.d.ts" />
 
-import React from 'react';
+import React, { useState } from 'react';
 import { shortenDID, shortenHandle } from '../api';
 
 import './account-header.css';
@@ -15,6 +15,26 @@ import { FullDID, FullHandle } from '../common-components/full-short';
  * }} _ 
  */
 export function AccountHeader({ className, account, onCloseClick }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleShortDIDClick = () => {
+    // Copy the shortDID to the clipboard
+    const modifiedShortDID = `did:plc:${account.shortDID}`;
+    const textField = document.createElement('textarea');
+    textField.innerText = modifiedShortDID;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+
+    // Show the "Copied to Clipboard" message
+    setIsCopied(true);
+
+    // Hide the message after a delay (e.g., 3 seconds)
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 3000);
+  };
 
   return (
     <div className={className}>
@@ -46,13 +66,16 @@ export function AccountHeader({ className, account, onCloseClick }) {
             !account.displayName ? undefined :
               <span className='account-handle'>
                 <span className='account-handle-at'>@</span>
+                <a href={`https:/bsky.app/profile/${account.shortHandle}`} target="_blank">
                 <FullHandle shortHandle={account.shortHandle} />
+              </a>
               </span>
           }
           {
             !account.shortDID ? undefined :
-              <span className='account-did'>
+              <span className='account-did' onClick={handleShortDIDClick}>
                 <FullDID shortDID={account.shortDID} />
+                {isCopied && <span style={{ marginLeft: '5px', color: 'green' }}>Copied to Clipboard</span>}
               </span>
           }
         </span>
