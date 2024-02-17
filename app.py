95c619a7344f5df02d1cda5e3290466fb6abc3f5
Jay <jburton6@aum.edu>
2024-02-17 11:07:44 -0600

    version increment: 4.0.16d // put links next to each other.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ config = config_helper.read_config()
 
 title_name = "ClearSky UI"
 os.system("title " + title_name)
-version = "4.0.15d"
+version = "4.0.16d"
 current_dir = os.getcwd()
 log_version = "ClearSky UI Version: " + version
 runtime = datetime.now()
