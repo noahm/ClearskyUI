884d44cc5fc7f9bbea46c72735530d65ba803b0c
Jay <jburton6@aum.edu>
2024-02-25 11:41:32 -0600

    add error handling.

--- app.py
+++ app.py
@@ -326,7 +326,10 @@ async def total_users_push_json():
 @rate_limit(30, timedelta(seconds=1))
 @app.route('/api/v1/serve/lists/stats/<path:filename>', methods=['GET'])
 async def serve_file(filename):
-    return await send_file(filename)
+    try:
+        return await send_file(filename)
+    except FileNotFoundError:
+        return jsonify({'message': 'File not found'}), 404
 
 
 # ======================================================================================================================
