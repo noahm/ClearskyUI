c7492fef43326c15020bdcbcf2c2375d3473f74f
Noah Manneschmidt <noah@manneschmidt.net>
2024-11-29 09:52:51 -0800

    use the separate total endpoint in the ui

--- src/detail-panels/blocking/index.js
+++ src/detail-panels/blocking/index.js
@@ -3,15 +3,18 @@
 import { useBlocklist } from '../../api';
 import { BlockPanelGeneric } from '../block-panel-generic';
 import { localise } from '../../localisation';
+import { useBlocklistCount } from '../../api/blocklist';
 
 /** @param {{ account: AccountInfo | { shortHandle: string; loading: true } }} _ */
 export function BlockingPanel({ account }) {
   const did = 'shortDID' in account ? account.shortDID : '';
   const blocklistQuery = useBlocklist(did);
+  const totalQuery = useBlocklistCount(did);
   return (
     <BlockPanelGeneric
       className="blocking-panel"
       blocklistQuery={blocklistQuery}
+      totalQuery={totalQuery}
       account={account}
       header={({ count }) => <>{localise('Blocking', { uk: 'Блокує' })}</>}
     />
